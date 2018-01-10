4f25cdde96f2d5ea96708f5cf3bbd53cf5199660
Marlene Vernet <marlene.vernet@epitech.eu>
2018-01-10 14:29:15 +0100

    mise en forme form

--- views/Account/Subscribe_form.php
+++ views/Account/Subscribe_form.php
@@ -1,24 +1,24 @@
 <div class="container text-center">
   <h2>Formulaire d'inscription</h2>
   <form method="POST">
-  <div class="cover">
-    <div class="col-md-5 offset-">
+  <div class="col-md-4 offset-5">
+    <div class="form-group">
       <label for="validationDefault01">Nom complet</label>
       <input type="text" class="form-control" id="validationDefault01" placeholder="fullname" name="fullname" required>
     </div>
-    <div class="col-md-5 offset-">
+    <div class="form-group">
       <label for="validationDefaultUsername">Pseudo</label>
         <input type="text" class="form-control" id="validationDefaultUsername" placeholder="displayname" name="displayname" required>
     </div>
-    <div class="col-md-5 offset">
+    <div class="form-group">
       <label for="validationDefault03">Mail</label>
       <input type="email" class="form-control" id="validationDefault03" placeholder="email"  name="mail" required>
     </div>
-    <div class="col-md-5 offset-">
+    <div class="form-group">
       <label for="validationDefault04">Mot de passe</label>
       <input type="password" class="form-control" id="validationDefault04" placeholder="password" name="password" required>
     </div>
-    <div class="col-md-5 offset-">
+    <div class="form-group">
       <button class="btn btn-secondary" type="submit">Submit form</button>
     </div>
   </div>
