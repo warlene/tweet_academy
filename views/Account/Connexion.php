a1cb1f6d09b22a4d602f745228f95907b96b0a98
marlene.corcher <marlene.corcher@epitech.eu>
2018-01-10 14:47:58 +0100

    before merge

--- views/Account/Connexion.php
+++ views/Account/Connexion.php
@@ -1,16 +1,16 @@
 <div class="container text-center">
   <h2>Connectez-vous</h2>
-    <div class="col-md-10 mb-5">
+    <div class="col-md-4 mb-5">
     <form>
-      <div class="col-sm-10 mb-5">
+      <div class="form-group">
         <label for="exampleDropdownFormEmail2">Email</label>
         <input type="email" class="form-control" id="exampleDropdownFormEmail2" name="mail" placeholder="email@example.com">
       </div>
-      <div class="col-sm-10 mb-5">
+      <div class="form-group">
         <label for="exampleDropdownFormPassword2">Mot de passe</label>
         <input type="password" class="form-control" id="exampleDropdownFormPassword2" name="password" placeholder="Password">
       </div>
-      <div class="col-sm-10 mb-5">
+      <div class="form-group">
         <button type="submit" class="btn btn-secondary">Connexion</button>
       </div>
     </form>
--- views/layout/Nav.php
+++ views/layout/Nav.php
@@ -32,8 +32,10 @@
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="<?php if(empty($_GET)){ ?>active<?php }?>"><a href="rooter.php">Tweet Factory</a></li>
-        <li class="<?php if(isset($_GET['action']) && $_GET['action']=='subscribe'){ ?>active<?php } ?>"><a href="rooter.php?controller=account&action=subscribe">Inscription</a></li>
-        <li class="<?php if(isset($_GET['action']) && $_GET['action']=='connexion'){ ?>active<?php } ?>"><a href="rooter.php?controller=account&action=connexion">Connexion</a></li>
+        <li class="<?php if(isset($_GET['action']) && $_GET['action']=='subscribe'){ ?>active<?php } ?>">
+        <a href="rooter.php?controller=account&action=subscribe">Inscription</a></li>
+        <li class="<?php if(isset($_GET['action']) && $_GET['action']=='connexion'){ ?>active<?php } ?>">
+        <a href="rooter.php?controller=account&action=connexion">Connexion</a></li>
       </ul>
     </div>
   </div>
