9dee0ce68ef0f730e508a2a722f9e587a309ba18
Marlene Vernet <marlene.vernet@epitech.eu>
2018-01-12 14:48:57 +0100

    profil

--- views/layout/Nav.php
+++ views/layout/Nav.php
@@ -15,18 +15,18 @@
   <div class="container-fluid">
     <div class="collapse navbar-collapse col-md-6" id="myNavbar">
       <ul class="nav navbar-nav">
+        <li><img src="images/factory.png" height="70" width="70"></li>
         <li class="<?php if(isset($_GET['action']) && $_GET['action']=='login'){ ?>active<?php } ?>">
           <a href="rooter.php">Accueil</a></li>
         <li class="<?php if(isset($_GET['action']) && $_GET['action']=='note'){ ?>active<?php } ?>">
           <a href="rooter.php?controller=account&action=note">Notifications</a></li>
         <li class="<?php if(isset($_GET['action']) && $_GET['action']=='message'){ ?>active<?php } ?>">
           <a href="rooter.php?controller=account&action=message">Messages</a></li>
-        <li><img src="images/factory.png" height="70" width="70"></li>
       </ul>
     </div>
       <form class="navbar-form navbar-right" role="search">
         <div class="form-group input-group">
-          <input type="text" class="form-control" placeholder="Recherche...">
+          <input type="text" class="form-control" placeholder="Recherche sur tweet factory...">
           <span class="input-group-btn">
             <button class="btn btn-default" type="button">
               <span class="glyphicon glyphicon-search"></span>
@@ -36,20 +36,18 @@
       </form>
       <ul class="nav navbar-nav navbar-right">
         <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Profil
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <li><a class="dropdown-item" target="_blank" href="views/7User/User_Profil.php"><?php echo $_SESSION['fullName'] ?></a></li>
-          <li><a class="dropdown-item" target="_blank" href="views/7User/User_Profil.php"><?php echo $_SESSION['displayName'] ?></a></li>
+          <li><a class="dropdown-item" href="rooter.php?controller=user&action=profil"><?php echo $_SESSION['fullName'] ?></a></li>
+          <li><a class="dropdown-item" href="rooter.php?controller=user&action=profil"><?php echo $_SESSION['displayName'] ?></a></li>
           <li role="separator" class="divider"></li>
-          <li><a class="dropdown-item" href="views/User/User_Profil.php">Profil</a></li>
+          <li><a class="dropdown-item" href="rooter.php?controller=user&action=profil">Profil</a></li>
           <li><a class="dropdown-item" href="views/User/User_Profil.php">Paramètres</a></li>
-
-
+          <li><a class="dropdown-item" href="rooter.php?controller=account&action=logout">Déconnexion</a></li>
         </ul>
       </li>
-        <li><a href="rooter.php?controller=account&action=logout">Deconnexion</a></li>
-      </ul>
+    </ul>
   </div>
 </nav>
